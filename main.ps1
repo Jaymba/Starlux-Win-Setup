9658ad82bbc144f5b8e885a91e26e058c1c1a1ae
Jay Stoltzfus <52546273+Jaymba@users.noreply.github.com>
2023-01-18 11:56:22 -0500

    Update and rename main.ps to main.ps1

--- main.ps1
+++ main.ps1
@@ -1,5 +1,10 @@
 
 
-
+#TO-DO check if winget is installed before installing and delete msixbundle after installation.
 Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "C:\Users\Admin\Downloads\WinGet.msixbundle"
 Add-AppxPackage "C:\Users\Admin\Downloads\WinGet.msixbundle"
+
+
+
+#uninstall winget
+winget uninstall Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
