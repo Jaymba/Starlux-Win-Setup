e1aafc433d542aa2a86a1d7728fb810808a14ff1
Jay Stoltzfus <52546273+Jaymba@users.noreply.github.com>
2023-01-18 12:03:45 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,4 +1,9 @@
-
+#Common Programs  ID
+#Adobe Acrobat:   Adobe.Acrobat.Reader.64-bit
+#LibreOffice:     TheDocumentFoundation.LibreOffice
+#GnuCash:         GnuCash.GnuCash
+#Gimp:            GIMP.GIMP
+#GoogleEarthPro   Google.EarthPro
 
 #TO-DO check if winget is installed before installing and delete msixbundle after installation.
 Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "C:\Users\Admin\Downloads\WinGet.msixbundle"
