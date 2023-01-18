3f69bdcf439f1de8d34b026cab5601647ae3f64f
Jay Stoltzfus <52546273+Jaymba@users.noreply.github.com>
2023-01-18 13:08:21 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -4,6 +4,12 @@
 #GnuCash:         GnuCash.GnuCash
 #Gimp:            GIMP.GIMP
 #GoogleEarthPro   Google.EarthPro
+#ImageGlass DuongDieuPhap.ImageGlass
+#TypingInstructor
+
+
+#TO-DO
+#Run windows update and reboot. Keep script running through reboot
 
 #TO-DO check if winget is installed before installing and delete msixbundle after installation.
 Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "C:\Users\Admin\Downloads\WinGet.msixbundle"
