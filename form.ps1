d75f19b75ff750c567fea45e3280191cc4eb72fc
Jay Stoltzfus <stoltzfusjay86@gmail.com>
2023-01-24 20:30:20 -0800

    et

--- form.ps1
+++ form.ps1
@@ -4,29 +4,40 @@
 # Create the form
 $form = New-Object Windows.Forms.Form
 
-
-#Set window size
-$form.ClientSize = '600,400'
+#Customize window 
+$form.ClientSize = '600,400' 
 
 #Set the dialog title
 $form.text = "Starlux Install Wizard"
 
-#Create Checkbox
-$checkbox = New-Object Windows.Forms.Checkbox
-$checkbox.Location = New-Object Drawing.Point 50,60
-$checkbox.Size = New-Object Drawing.Point 200,30
-$checkbox.Text = "Testing"
+#Create Checkboxs
+$g_checkbox = New-Object Windows.Forms.Checkbox
+$g_checkbox.Location = New-Object Drawing.Point 50,30
+$g_checkbox.Size = New-Object Drawing.Point 150,30
+$g_checkbox.Text = "Guardian Group Policy"
+
+$v_checkbox = New-Object Windows.Forms.Checkbox
+$v_checkbox.Location = New-Object Drawing.Point 50,60
+$v_checkbox.Size = New-Object Drawing.Point 150,30
+$v_checkbox.Text = "Video"
+
+$i_checkbox = New-Object Windows.Forms.Checkbox
+$i_checkbox.Location = New-Object Drawing.Point 50,90
+$i_checkbox.Size = New-Object Drawing.Point 150,30
+$i_checkbox.Text = "Images"
+
+#Create Dropdown
 
 # Create the label control and set text, size and location
-$label = New-Object Windows.Forms.Label
-$label.Location = New-Object Drawing.Point 50,30
-$label.Size = New-Object Drawing.Point 200,15
-$label.text = "Enter your name and click the button"
+#$label = New-Object Windows.Forms.Label
+#$label.Location = New-Object Drawing.Point 50,30
+#$label.Size = New-Object Drawing.Point 200,15
+#$label.text = "Enter your name and click the button"
 
 # Create TextBox and set text, size and location
-$textfield = New-Object Windows.Forms.TextBox
-$textfield.Location = New-Object Drawing.Point 50,60
-$textfield.Size = New-Object Drawing.Point 200,30
+#$textfield = New-Object Windows.Forms.TextBox
+#$textfield.Location = New-Object Drawing.Point 50,60
+#$textfield.Size = New-Object Drawing.Point 200,30
 
 # Create Button and set text and location
 $button = New-Object Windows.Forms.Button
@@ -34,6 +45,10 @@ $button.text = "Greeting"
 $button.Location = New-Object Drawing.Point 100,90
 
 # Set up event handler to extarct text from TextBox and display it on the Label.
+
+
+
+
 $button.add_click({
 
 $label.Text = "Hello " + $textfield.text
@@ -41,9 +56,11 @@ $label.Text = "Hello " + $textfield.text
 })
 
 # Add the controls to the Form
-$form.controls.add($button)
-$form.controls.add($label)
-$form.controls.add($checkbox)
+#$form.controls.add($button)
+#$form.controls.add($label)
+$form.controls.add($g_checkbox)
+$form.controls.add($v_checkbox)
+$form.controls.add($i_checkbox)
 
 # Display the dialog
 $form.ShowDialog()
--- main.ps1
+++ main.ps1
@@ -13,11 +13,47 @@
 #Add options to disable sound, video, and images.
 
 #TO-DO check if winget is installed before installing and delete msixbundle after installation.
-Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "C:\Users\Admin\Downloads\WinGet.msixbundle"
-Add-AppxPackage "C:\Users\Admin\Downloads\WinGet.msixbundle"
+
+if(-Not (Get-Command winget -errorAction SilentlyContinue)){
+    Invoke-WebRequest -Uri "https://github.com/microsoft/winget-cli/releases/download/v1.1.12653/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile ".\WinGet.msixbundle"
+    Add-AppxPackage ".\WinGet.msixbundle"    
+}
 
 #Use flag --accept-source-agreements for first winget cmd
 
 
+#Menus
+function PDF-Menu 
+{
+    param(
+        [string]$Title = 'Select a PDF Reader'
+    )
+    Clear-Host
+    Write-Host "=========================$Title==========================="
+
+    Write-Host "1: Adobe Acrobat"
+    Write-Host "2: Nitro"
+    Write-Host "q: Quit"
+}
+
+
+
+#Menu Logic
+PDF-Menu
+$pdfReader = Read-Host "Enter your selection"
+
+switch($pdfReader)
+{
+    '1'{
+        winget install Adobe.Acrobat.Reader.64-bit
+    } '2'{
+        'You selected Nitro Reader'
+    } 'q'{
+        return
+    }
+}
+
+
+
 #uninstall winget
-winget uninstall Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
+#winget uninstall Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
