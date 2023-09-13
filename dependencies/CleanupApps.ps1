c15eb75b326ba531187c5c7044632672477f6ba1
Alex N <alex.n@starluxpro.com>
2023-09-13 16:43:31 -0400

    Modified CleanupApps.ps1 to not remove winget from the system

--- dependencies/CleanupApps.ps1
+++ dependencies/CleanupApps.ps1
@@ -1,4 +1,4 @@
-$goodapps = "communicationsapps|weather|maps|sketch|calculator|stickynotes|alarms|paint|store|edge|photos|extension|vclibs|runtime" #Sets a variable thats used for excluding to-be removed apps
+$gioodapps = "communicationsapps|weather|maps|sketch|calculator|stickynotes|alarms|paint|store|edge|photos|extension|vclibs|runtime|winget|desktopappinstaller" #Sets a variable thats used for excluding to-be removed apps
 
 get-appxprovisionedpackage -online |where {$_.displayname -notmatch $goodapps } |select displayname #Just shows you what is going to be removed
 get-appxprovisionedpackage -online |where {$_.displayname -notmatch $goodapps } | remove-appxprovisionedpackage -online #This does the actual removal from the output of the above command 
